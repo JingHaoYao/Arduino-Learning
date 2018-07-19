9654e521fde8b08208f9c766c52f060cc9746362
Jing Yao <jinghaoyao_@hotmail.com>
2018-07-19 17:11:30 -0400

    Add files via upload

---
+++ _8LEDLightShow_July_19th_2018.ino
@@ -0,0 +1,65 @@
+int latchPin = 11;
+int clockPin = 9;
+int dataPin = 12;
+
+byte leds = 0;
+
+void setup() {
+pinMode(latchPin,OUTPUT);
+pinMode(dataPin,OUTPUT);
+pinMode(clockPin,OUTPUT);
+
+}
+
+void switchUpdate(){
+  digitalWrite(latchPin,LOW);
+  shiftOut(dataPin,clockPin,LSBFIRST,leds);
+  digitalWrite(latchPin,HIGH);
+}
+
+void rowFill1(){
+  leds = 0;
+  switchUpdate();
+  for(int i = 0;i<8;i++){
+    bitSet(leds,i);
+    switchUpdate();
+    delay(100);
+  }
+}
+
+void rowUnFill1(){
+  leds = 255;
+  switchUpdate();
+  for(int k = 0;k<8;k++){
+    bitClear(leds,k);
+    switchUpdate();
+    delay(100);
+  }
+}
+
+void rowFill2(){
+  leds = 128;
+  switchUpdate();
+  for(int l = 7;l>=0;l--){
+    bitSet(leds,l);
+    switchUpdate();
+    delay(100);
+  }
+}
+
+void rowUnFill2() {
+  leds = 255;
+  switchUpdate();
+  for(int n = 7;n>=0;n--){
+    bitClear(leds,n);
+    switchUpdate();
+    delay(100);
+  }
+}
+
+void loop() {
+ rowFill1();
+ rowUnFill1();
+ rowFill2();
+ rowUnFill2();
+}
